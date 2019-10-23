bb2a013bfeea27c6e0301f0d188547402f72c00e
txoof <aaron.ciuffo@gmail.com>
2019-10-23 12:07:32 +0200

    add explicit load of scanLMS

--- lmsquery/__init__.py
+++ lmsquery/__init__.py
@@ -1,6 +1,6 @@
 from . import const
 from . import lmsquery
-from . import scanLMS
+from .scanLMS import scanLMS
 
 def LMSQuery(host=const.LMS_HOST, port=const.LMS_PORT, player_id=""):
     return lmsquery.LMSQuery(host, port, player_id)
