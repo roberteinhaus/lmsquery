775da439d56ce5f1febf61ddfae2031d75688885
txoof <aaron.ciuffo@gmail.com>
2019-10-26 11:29:30 +0200

    update __init__ file to match explicit loads for Python3

--- lmsquery/__init__.py
+++ lmsquery/__init__.py
@@ -1,6 +1,6 @@
 from . import const
-from . import lmsquery
+from .lmsquery import LMSQuery
 from .scanLMS import scanLMS
 
-def LMSQuery(host=const.LMS_HOST, port=const.LMS_PORT, player_id=""):
-    return lmsquery.LMSQuery(host, port, player_id)
+#def LMSQuery(host=const.LMS_HOST, port=const.LMS_PORT, player_id=""):
+#    return lmsquery.LMSQuery(host, port, player_id)
